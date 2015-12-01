05bfe1fe02512eec4e8863c953f308bef8e52663
Sebastian Pawluś <sebastian.pawlus@gmail.com>
2015-12-01 13:36:18 +0100

    init

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM ubuntu
+
+RUN apt-get -y update
+
+RUN apt-get install -y software-properties-common
+RUN apt-get install -y wget
+RUN apt-get install -y gcc
+RUN apt-get install -y python-dev
+
+RUN add-apt-repository ppa:pypy/ppa
+
+RUN apt-get -y update
+
+RUN apt-get install -y pypy
+
+RUN wget https://bootstrap.pypa.io/get-pip.py
+RUN pypy get-pip.py
+
+# docker run -v /vagrant/pippache:/pipcache pypyspace pypy -m pip --cache-dir=/pipcache install django
---
+++ main.py
@@ -0,0 +1,53 @@
+from multiprocessing import Pool
+
+import subprocess
+import os
+import json
+import shutil
+import xmlrpclib
+
+PYPI = xmlrpclib.ServerProxy('https://pypi.python.org/pypi')
+PATH = "logs"
+PIP_CACHE = "/home/vagrant/pipcache"
+DOWNLOAD_PROCESSES = 10
+
+
+def thing(args):
+    name, count = args
+    p = subprocess.Popen('docker run -v %s:/pipcache pypyspace pypy -m pip --cache-dir=/pipcache install %s' % (PIP_CACHE, name),
+                         shell=True,
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.STDOUT)
+    stdout, _ = p.communicate()
+
+    print "=" * 30, name, p.returncode, "=" * 30
+    print stdout
+
+    return name, p.returncode, stdout, count
+
+
+if __name__ == '__main__':
+
+    if os.path.exists(PATH):
+        shutil.rmtree(PATH)
+
+    os.makedirs(PATH)
+
+    pool = Pool(processes=DOWNLOAD_PROCESSES)
+    results = pool.map(thing, PYPI.top_packages(1000))
+    pool.close()
+    pool.join()
+
+    index = []
+
+    for name, status, output, count in results:
+        output_log = os.path.join(PATH, '%s.log' % name)
+
+        with open(output_log, 'w') as f:
+            f.write(output)
+
+        index.append((name, {"status": status,
+                             "count": count,
+                             "log": output_log}))
+
+    json.dump(index, open("index.json", "w"), indent=2)
