4a3033a5fc1c79ea6be7ed0d233b3ed7d20be3e4
Alex Gaynor <alex.gaynor@gmail.com>
2015-12-18 14:59:28 -0500

    Add a note baout psycopg2

--- web/notes.js
+++ web/notes.js
@@ -1,4 +1,5 @@
 var notes = {
     "lxml": "Known not to work due to problems with CPython C API emulation layer. See <a href='https://github.com/mithrandi/lxml/tree/cffi'>lxml-cffi</a> for a replacement",
     "ssl": "Not needed in python >= 2.6",
+    "psycopg2": "psycopg2cffi can be used instead"
 };
