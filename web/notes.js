3988fb9311812d01ad562dabee3d98a196cb10aa
Maciej Fijalkowski <fijall@gmail.com>
2015-12-18 14:37:52 +0200

    info about lxml

--- web/notes.js
+++ web/notes.js
@@ -1,3 +1,3 @@
 var notes = {
-    simplejson: "test <b>test</b>"
+    "lxml": "Known not to work due to problems with CPython C API emulation layer. See <a href='https://github.com/amauryfa/lxml/tree/cffi'>lxml-cffi</a> for a replacement"
 };
