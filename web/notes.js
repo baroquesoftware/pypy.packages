30e60278fe6fed18fabc7ef3420fb459d6faa2b9
Maciej Fijalkowski <fijall@gmail.com>
2015-12-18 15:07:58 +0200

    write a note about ssl

--- web/notes.js
+++ web/notes.js
@@ -1,3 +1,4 @@
 var notes = {
-    "lxml": "Known not to work due to problems with CPython C API emulation layer. See <a href='https://github.com/mithrandi/lxml/tree/cffi'>lxml-cffi</a> for a replacement"
+    "lxml": "Known not to work due to problems with CPython C API emulation layer. See <a href='https://github.com/mithrandi/lxml/tree/cffi'>lxml-cffi</a> for a replacement",
+    "ssl": "Not needed in python >= 2.6",
 };
