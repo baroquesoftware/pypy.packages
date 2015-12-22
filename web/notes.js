bdbc502581112478df249eaa0c2dd5fb919d33ed
Alex Gaynor <alex.gaynor@gmail.com>
2015-12-22 13:01:21 -0500

    added a link

--- web/notes.js
+++ web/notes.js
@@ -1,5 +1,5 @@
 var notes = {
     "lxml": "Known not to work due to problems with CPython C API emulation layer. See <a href='https://github.com/mithrandi/lxml/tree/cffi'>lxml-cffi</a> for a replacement",
     "ssl": "Not needed in python >= 2.6",
-    "psycopg2": "psycopg2cffi can be used instead"
+    "psycopg2": "<a href='https://pypi.python.org/pypi/psycopg2cffi'>psycopg2cffi</a> can be used instead"
 };
