8045f873a34d49e6db4e9019f60f0e5ef9d832a4
Armin Rigo <arigo@tunes.org>
2016-02-21 22:34:11 +0100

    matplotlib blurb

--- web/notes.js
+++ web/notes.js
@@ -2,5 +2,6 @@ var notes = {
     "lxml": "Known not to work due to problems with CPython C API emulation layer. See <a href='https://pypi.python.org/pypi/lxml-cffi'>lxml-cffi</a> for a replacement",
     "ssl": "Not needed in python >= 2.6",
     "psycopg2": "<a href='https://pypi.python.org/pypi/psycopg2cffi'>psycopg2cffi</a> can be used instead",
-    "numpy": "Work in progress.  See <a href='http://pypy.org/download.html#installing-numpy'>instructions.</a>"
+    "numpy": "Work in progress.  See <a href='http://pypy.org/download.html#installing-numpy'>instructions.</a>",
+    "matplotlib": "See <a href='http://morepypy.blogspot.ch/search?q=matplotlib&max-results=20&by-date=true'>PyPy's blog.</a>"
 };
