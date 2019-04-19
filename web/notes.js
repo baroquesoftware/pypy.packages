b16a799ef0b665be9d03df8073b997759267344b
Sebastian Pawluś <sebastian.pawlus@gmail.com>
2019-04-20 00:10:19 +0200

    JS mising commas

--- web/notes.js
+++ web/notes.js
@@ -1,8 +1,8 @@
 var notes = {
     "ssl": "Not needed in python >= 2.6",
     "psycopg2": "<a href='https://pypi.python.org/pypi/psycopg2cffi'>psycopg2cffi</a> can be used instead",
-    "gevent": "Release 1.1 supports PyPy."
-    "mysql-python": "Does not support python3"
-    "cheetah": "Does not support python3"
+    "gevent": "Release 1.1 supports PyPy.",
+    "mysql-python": "Does not support python3",
+    "cheetah": "Does not support python3",
     "functools32": "Not needed in python > 2"
 };
